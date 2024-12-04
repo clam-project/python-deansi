f965a2d9c9623b30fc2c0cbe0f6cba0b4f6c7a25
David García Garzón <voki@canvoki.net>
2024-12-04 09:57:55 +0100

    🏳 py2 and setup.py back
    It is either having py2 or pyproject for no gain by now
    
    This reverts commit fb94568d85855b2fd680a184b3989df6c524dce1.

---
+++ setup.py
@@ -0,0 +1,43 @@
+#!/usr/bin/env python
+# -*- coding: utf8 -*-
+from setuptools import setup, find_packages
+
+readme = open("README.md").read()
+
+setup(
+	name = "deansi",
+	version = "1.2",
+	description = "ANSI codes to HTML converter",
+	author = "David García Garzón",
+	author_email = "voki@canvoki.net",
+	url = 'https://github.com/GuifiBaix/python-deansi',
+	long_description_content_type='text/markdown',
+	long_description = readme,
+	license = 'GNU General Public License v3 or later (GPLv3+)',
+	packages=find_packages(exclude=['*[tT]est*']),
+	py_modules=[
+		'deansi',
+	],
+	entry_points={
+		'console_scripts': [
+			'deansi=deansi:main',
+		]
+	},
+	include_package_data = True,
+	test_suite = 'deansi_test',
+	classifiers = [
+		'Programming Language :: Python',
+		'Programming Language :: Python :: 2',
+		'Programming Language :: Python :: 3',
+		'Topic :: Software Development :: Libraries :: Python Modules',
+		'Topic :: System :: Logging',
+		'Topic :: Text Processing :: Filters',
+		'Topic :: Text Processing :: Markup :: HTML',
+		'Environment :: Console',
+		'Intended Audience :: Developers',
+		'Development Status :: 5 - Production/Stable',
+		'License :: OSI Approved :: GNU Affero General Public License v3 or later (AGPLv3+)',
+		'Operating System :: OS Independent',
+	],
+)
+
